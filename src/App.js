37901bc42742c9f806f5787fa922b2c808b8141e
unknown <g00361129@gmit.ie>
2020-02-18 14:37:30 +0000

    First Master Push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+// import DefaultAlbumsLibrary from "./components/defaultAlbumsLibrary";
+// import AddAlbum from './components/addAlbum';
+// import EditAlbum from "./components/editAlbum";
+// import AlbumsLibrary from './components/albumsLibrary';
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from "react-bootstrap";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar className="navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">PcBuilderPro</a>
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/buildPC">Build a PC</Nav.Link>
+            <Nav.Link href="/contacts">Contacts</Nav.Link>
+          </Nav>
+        </Navbar>
+        <br/>
+        <Switch>
+          {/* <Route exact path='/' component={DefaultAlbumsLibrary}/>
+          <Route exact path='/addAlbum' component={AddAlbum}/>
+          <Route exact path='/albumsLibrary' component={AlbumsLibrary}/>
+          <Route exact path='/editAlbum/:id' component={EditAlbum}/> */}
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
