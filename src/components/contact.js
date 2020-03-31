97bf538ab337a76aeb64dbfb841e60baeee89989
JackH97 <37179000+JackH97@users.noreply.github.com>
2020-03-31 15:29:30 +0100

    Did homepage, about and contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,37 +2,69 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import '../App.css';
 import Form from "react-bootstrap/Form";
+//import Modal from 'react-bootstrap/Modal'
+
+/* function PopUp(){
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true); */
 
 class Contact extends React.Component{
+  
 
   render(){
     return(
+      
       <div class="bodyFormat">
-          <br/>
           <h1>Contact Us!</h1>
           <h2>If you have any queries, questions or feedback, 
             do not hesitate to contact us using the form below, 
             and we will get back to you as soon as possible!
           </h2>
           <br/>
-          <div class="centerElement">
-          <Form>
-            <Form.Group>
-              <Form.Label>Email</Form.Label>
-              <Form.Control as="input" placeholder="Email"/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Feedback</Form.Label>
-              <Form.Control as="textarea" placeholder="Feedback"/>
-            </Form.Group>
 
-            <Button variant="primary" type="submit">Submit</Button>
-            <br/><br/>
-          </Form>
+          <div class="centerElement">
+            <Form>
+              <Form.Group>
+                <Form.Label>Name</Form.Label>
+                <Form.Control as="input" placeholder="Email"/>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control as="textarea" placeholder="Feedback"/>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Feedback</Form.Label>
+                <Form.Control as="textarea" placeholder="Feedback"/>
+              </Form.Group>
+              </Form>
+           
+           
+              {/* <Button variant="primary" onClick={handleShow}>
+              Launch demo modal
+              </Button>   
+              
+
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
+              </Modal> */}
+            </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
-}
+//}
+//render(<PopUp />);
 
 export default Contact;
--- src/components/home.js
+++ src/components/home.js
@@ -9,35 +9,57 @@ class About extends React.Component{
     return(
       <div class="bodyFormat">
         <div class="slideshowFormat">
+          <br></br>
+          <h1>PC builder Pro</h1>
+          <br></br>
+          <br></br>
+          <h3>Click on image(s) to see product further</h3>
           <Carousel>
             <Carousel.Item>
-              <img 
+              <a href="https://www.nvidia.com/en-gb/geforce/graphics-cards/rtx-2080-ti/">
+              <img
                 src="https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/geforce-rtx-turing/2080-ti/gallery/geforce-rtx-2080-ti-gallery-b.jpg"
                 alt="GeForce RTX 2080 Ti"
                 class="imageFormat"/>
+                </a>
               <Carousel.Caption><h3>GeForce RTX 2080 Ti</h3></Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
+            <a href="https://www.amd.com/en/products/cpu/amd-ryzen-threadripper-3990x">
               <img 
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.notebookcheck.net%2Ffileadmin%2FNotebooks%2FNews%2F_nc3%2FAMD_Ryzen_Threadripper_CPUs.png"
                 alt="AMD Threadripper 3990x"
                 class="imageFormat"/>
+                </a>
               <Carousel.Caption><h3>AMD Threadripper 3990x</h3></Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
+              <a href="https://www.asrock.com/mb/AMD/TRX40%20Taichi/">
               <img 
                 src="https://www.asrock.com/images/index_TRX40Taichi.jpg"
                 alt="ASRock TRX40 Taichi"
                 class="imageFormat"/>
+                </a>
               <Carousel.Caption><h3>ASRock TRX40 Taichi</h3></Carousel.Caption>
             </Carousel.Item>
           </Carousel>
+          <br></br>
+          <img
+          src="https://img.pngio.com/computer-update-delplogdelplog-funny-computer-png-814_555.png"
+          alt="Homepage pic"
+          height="300"
+          width="300"
+          />
+          <br></br>
+          <h2>It is {new Date().toLocaleTimeString()}.</h2>
         </div>
       </div>
+      
     )
   }
 }
 
 export default About;
+
